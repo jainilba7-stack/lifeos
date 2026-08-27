c243a2000b6490560551a7e5b45d8ba541e78d8b
jainilba7-stack <jainilba7@gmail.com>
2026-08-27 13:59:32 +0530

    go tehn

--- backend/src/routes/documentRoutes.js
+++ backend/src/routes/documentRoutes.js
@@ -1,15 +1,46 @@
+//const express = require('express');
+//const router = express.Router();
+//const documentController = require('../controllers/documentController');
+//const { protect } = require('../middleware/authMiddleware');
+//const { upload } = require('../middleware/uploadMiddleware');
+
+//router.use(protect);
+
+//router
+// .route('/')
+//.get(documentController.getDocuments)
+//.post(upload.single('file'), documentController.uploadDocument);
+
+//router.delete('/:id', documentController.deleteDocument);
+
+//module.exports = router;
 const express = require('express');
 const router = express.Router();
 const documentController = require('../controllers/documentController');
 const { protect } = require('../middleware/authMiddleware');
-const { upload } = require('../middleware/uploadMiddleware');
+const { upload, processFileUpload } = require('../middleware/uploadMiddleware');
 
 router.use(protect);
 
 router
   .route('/')
   .get(documentController.getDocuments)
-  .post(upload.single('file'), documentController.uploadDocument);
+  .post(
+    upload.single('file'),
+    async (req, res, next) => {
+      try {
+        if (req.file) {
+          const hostUrl = `${req.protocol}://${req.get('host')}`;
+          const uploadedFile = await processFileUpload(req.file, hostUrl);
+          req.body.fileUrl = uploadedFile.url;
+        }
+        next();
+      } catch (err) {
+        next(err);
+      }
+    },
+    documentController.uploadDocument
+  );
 
 router.delete('/:id', documentController.deleteDocument);
 
