4a2a9cff9955ac0a16958d7fe97af899f8840027
jainilba7-stack <jainilba7@gmail.com>
2026-08-27 14:08:29 +0530

    move ahead

--- backend/src/routes/documentRoutes.js
+++ backend/src/routes/documentRoutes.js
@@ -1,20 +1,43 @@
-//const express = require('express');
-//const router = express.Router();
-//const documentController = require('../controllers/documentController');
-//const { protect } = require('../middleware/authMiddleware');
-//const { upload } = require('../middleware/uploadMiddleware');
-
-//router.use(protect);
-
-//router
-// .route('/')
-//.get(documentController.getDocuments)
-//.post(upload.single('file'), documentController.uploadDocument);
-
-//router.delete('/:id', documentController.deleteDocument);
-
-//module.exports = router;
 const express = require('express');
+const router = express.Router();
+const documentController = require('../controllers/documentController');
+const { protect } = require('../middleware/authMiddleware');
+const { upload } = require('../middleware/uploadMiddleware');
+
+router.use(protect);
+
+router
+  .route('/')
+  .get(documentController.getDocuments)
+  .post(upload.single('file'), documentController.uploadDocument);
+
+router.delete('/:id', documentController.deleteDocument);
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const express = require('express');
 const router = express.Router();
 const documentController = require('../controllers/documentController');
 const { protect } = require('../middleware/authMiddleware');
@@ -45,3 +68,6 @@ router
 router.delete('/:id', documentController.deleteDocument);
 
 module.exports = router;
+
+
+ */
